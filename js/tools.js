2a24c8847752d63586212368189f0b47624fcea5
Mohamad Zaki Mustafa <mohdzaki04@gmail.com>
2017-10-10 22:43:36 +0800

    update page

--- js/tools.js
+++ js/tools.js
@@ -1,28 +1,51 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
 
-	$('body').removeClass().addClass('green-style');
 
-	// WOW Initialization
-	new WOW().init();
+    $('body').removeClass().addClass('green-style');
 
-	// SideNav Initialization
-	$(".button-collapse").sideNav();	
+    // WOW Initialization
+    new WOW().init();
 
-	//soalanModal
-	$('.list-group > a').click(function(event){
-	    event.preventDefault();
-	    var e = $(this);
-	    var index = e.data('number');
-	    var title = e.data('title');    
-	    var body = e.data('body');
-	    
-	    $('#myModalLabel').html("Soalan "+index);
-	    $('#myModalTitle').html(title);
-	    $('#myModalBody').html(body);
-	});
-	$('#soalanModal').on('hidden.bs.modal', function (e) {
-	  $('#myModalTitle').html();
-	    $('#myModalBody').html();
-	});
+    // SideNav Initialization
+    $(".button-collapse").sideNav();
+
+    //soalanModal
+    $('.list-group > a').click(function (event) {
+        event.preventDefault();
+        var e = $(this);
+        var index = e.data('number');
+        var title = e.data('title');
+        var body = e.data('body');
+
+        $('#myModalLabel').html("Soalan " + index);
+        $('#myModalTitle').html(title);
+        $('#myModalBody').html(body);
+    });
+    $('#soalanModal').on('hidden.bs.modal', function (e) {
+        $('#myModalTitle').html();
+        $('#myModalBody').html();
+    });
+
+    //soalanModal
+    $('.jabatan > a').click(function (event) {
+        event.preventDefault();
+        var e = $(this);
+        //var index = e.data('number');
+        var title = e.data('title');
+        var alamat = e.data('alamat');
+        var tel = e.data('tel');
+        var fax = e.data('fax');
+        
+        $('#myModalJabatanTitle').html(title);
+        $('#myModalJabatanAlamat').html(alamat);
+        $('#myModalJabatanTel').html(tel);
+        $('#myModalJabatanFax').html(fax);
+    });
+
+    $('#jabatanModal').on('hidden.bs.modal', function (e) {
+        $('#myModalJabatanTitle').html();
+        $('#myModalJabatanAlamat').html();
+        $('#myModalJabatanTel').html();
+        $('#myModalJabatanFax').html();
+    });
 });
